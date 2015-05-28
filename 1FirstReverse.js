6b5fddc30a70523414c870591d63b5a9bdfab9c1
Ting Deng <maidenbelize@gmail.com>
2015-05-27 17:38:34 -0700

    first commit

---
+++ 1FirstReverse.js
@@ -0,0 +1,10 @@
+var FirstReverse = function (str) {
+    return str.split("").reverse().join("");
+};
+
+function FirstReverse2 (str) {
+    return str.split("").reverse().join("");
+}
+
+FirstReverse("string");
+FirstReverse2("string");
